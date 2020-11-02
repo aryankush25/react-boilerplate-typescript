e7fd682eacdadd20205b1b0dd46c084e5c54bdaa
Aryan Agarwal <aryankush025@gmail.com>
2020-11-03 02:16:51 +0530

    selector bugfix

--- src/store/utils/selectors.ts
+++ src/store/utils/selectors.ts
@@ -1,7 +1,8 @@
 import _ from 'lodash';
 import StoreState from './StoreTypes';
 
-export const getUserTokensToken = (state: StoreState) => ({
-  accessToken: _.get(state, 'user.userStatus.accessToken', ''),
-  refreshToken: _.get(state, 'user.userStatus.refreshToken', '')
+export const getUserTokens = (state: StoreState) => ({
+  username: _.get(state, 'userData.userData.username', ''),
+  accessToken: _.get(state, 'userData.userData.accessToken', ''),
+  refreshToken: _.get(state, 'userData.userData.refreshToken', '')
 });
